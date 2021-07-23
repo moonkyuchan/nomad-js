9b67b1c7f1e49cb9deee8742d0e8a8d76da8b05f
문규찬 <lazychan90@gmail.com>
2021-07-23 15:13:09 +0900

    filter 사용해서 todolist  delete 기능 작업

--- JS/toDo.js
+++ JS/toDo.js
@@ -2,19 +2,30 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+let toDosArray = [];
+const TODOS_KEY = "toDo";
+
+function pushTodoList() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDosArray));
+}
+
 function deleteTodo(event) {
   const deleteLiTag = event.target.parentElement;
   deleteLiTag.remove();
+  console.log(typeof deleteLiTag.id);
+  toDosArray = toDosArray.filter((el) => el.id !== parseInt(deleteLiTag.id));
+  pushTodoList();
 } // 여러가지 todolist들의 항목을 지정해서 삭제해주기 위해 target 설정
 
 function paintTodo(todoValue) {
   const liTag = document.createElement("li");
+  liTag.id = todoValue.id;
   todoList.appendChild(liTag);
   const spanTag = document.createElement("span");
   const buttonTag = document.createElement("button");
   liTag.appendChild(spanTag);
   liTag.appendChild(buttonTag);
-  spanTag.innerText = todoValue;
+  spanTag.innerText = todoValue.text;
   buttonTag.innerText = "✖️";
   buttonTag.addEventListener("click", deleteTodo);
 }
@@ -23,7 +34,21 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const todoValue = todoInput.value; //todoValue 라는 변수에 value는 위에서 저장시키고
   todoInput.value = ""; //여기서 엔터키 누를시 value 없어짐... 저장값이 사라지는거 아님!
-  paintTodo(todoValue);
+  const todoValueObj = {
+    text: todoValue,
+    id: Date.now(),
+  };
+  toDosArray.push(todoValueObj);
+  paintTodo(todoValueObj);
+  pushTodoList();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedToDo = localStorage.getItem(TODOS_KEY);
+
+if (savedToDo) {
+  const parsedTodo = JSON.parse(savedToDo);
+  toDosArray = parsedTodo;
+  parsedTodo.forEach(paintTodo);
+}
